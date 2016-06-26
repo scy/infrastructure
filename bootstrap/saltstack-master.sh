7656df2500d886956c62aadb4f90f7714d27304a
Tim Weber <scy@scy.name>
2016-06-26 14:39:01 +0200

    Add WIP code to bootstrap a SaltStack master
    At the moment, I’m not quite sure whether I want one at all.

---
+++ bootstrap/saltstack-master.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+gpgkey='../keys/public/saltstack.asc'
+
+msg() {
+	printf 'bootstrap saltstack master: %s\n' "$*"
+}
+
+die() {
+	msg "$*" >&2
+	exit 1
+}
+
+[ -r "$gpgkey" ] || die "cannot find or read OpenPGP key: $gpgkey"
+
+host="$1"
+[ -n "$host" ] || die 'please supply the host name to bootstrap as a parameter'
+
+run() {
+	ssh -o VisualHostKey=no "$host" "$@"
+}
+
+msg 'checking connection…'
+run true || die "SSH connection to $host did not work"
+
+msg 'installing salt…'
+run 'echo "Apt::Install-Suggests \"0\";" > /etc/apt/apt.conf.d/50-no-suggests && apt-key add - && echo "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" > /etc/apt/sources.list.d/saltstack.list && apt-get update && apt-get -y install salt-master salt-minion' < "$gpgkey"
