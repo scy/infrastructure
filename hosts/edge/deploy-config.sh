72be6e288a1d3ab4ee097b1b7d257f267c1387e0
Tim Weber <scy@scy.name>
2016-10-03 14:30:11 +0200

    Generate DHCP and DNS files for dnsmasq
    This fixes <http://serverfault.com/q/806745/50937>.

--- hosts/edge/deploy-config.sh
+++ hosts/edge/deploy-config.sh
@@ -12,5 +12,12 @@ if [ ! -x "$me" ]; then
 	exit 1
 fi
 
+msg 'generating DHCP and DNS config'
+awk -F , '{ sub(/ *#.*$/, "") } /./ { print $1 "," $2 }' hosts.txt > etc/dnsmasq.staticdhcp
+awk -F , '{ sub(/ *#.*$/, "") } /./ { print $2 " " $3 ".b17.scy.name" }' hosts.txt > etc/dnsmasq.hosts
+
 msg 'rsyncing config into place'
 sudo rsync -rtlib --backup-dir="$backupdir" etc /
+
+msg 'restarting dnsmasq'
+sudo service dnsmasq restart
